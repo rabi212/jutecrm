25e03e1c07e8385d3571c29d6273c18f0fd1e86d
rabi <rabisanfui96@gmail.com>
2026-08-13 18:00:03 +0530

    Add backend and frontend

---
+++ backend/backend/config/seed.js
@@ -0,0 +1,6 @@
+// Database seeding disabled by request. Database will start completely blank.
+async function seedInitialData() {
+  console.log('Database seeding is disabled. Starting with a blank database.');
+}
+
+module.exports = { seedInitialData };
