59e362da017d74ca71cbee29725493261bbf076c
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-13 18:06:30 +0530

    Delete fronend directory

--- fronend/backend/config/seed.js
+++
@@ -1,6 +0,0 @@
-// Database seeding disabled by request. Database will start completely blank.
-async function seedInitialData() {
-  console.log('Database seeding is disabled. Starting with a blank database.');
-}
-
-module.exports = { seedInitialData };
